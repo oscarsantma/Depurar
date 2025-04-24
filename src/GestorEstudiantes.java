5ae84d03c23795338e7be96bd3703a92ccd06ab3
santiago.maosc <santiago.maosc21@cordoba.salesianos.edu>
2025-04-24 20:09:30 +0200

    ERROR 1 solucionado

--- src/GestorEstudiantes.java
+++ src/GestorEstudiantes.java
@@ -7,7 +7,7 @@ public class GestorEstudiantes {
     // Calcula la nota media de un estudiante
     public static double calcularNotaMedia(Estudiante estudiante) {
         double suma = 0;
-        for (int i = 0; i <= estudiante.getNotas().length; i++) { // Error: índice fuera de rango
+        for (int i = 0; i < estudiante.getNotas().length; i++) { // Error: índice fuera de rango
             suma += estudiante.getNotas()[i];
         }
         return suma / estudiante.getNotas().length; // Error si el array está vacío
